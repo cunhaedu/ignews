66f65616d91b04ed714eae5a09006bb13e4866e0
Eduardo Assunção <eduardo.assuncao@gestaokaizen.com.br>
2022-10-30 20:31:37 -0300

    feat(posts): implements posts page and integrate with prismic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
+import { PrismicProvider } from '@prismicio/react';
 import { SessionProvider } from 'next-auth/react';
+import { PrismicPreview } from '@prismicio/next';
+import Link from 'next/link';
 
+import { prismicRepositoryName } from '../services/prismic';
 import { Header } from '../components/Header';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
+      <PrismicProvider
+        internalLinkComponent={({ href, ...props }) => (
+          <Link href={href}>
+            <a {...props} />
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={prismicRepositoryName}>
+          <Header />
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>
     </SessionProvider>
   )
 }
